e04b8946dea23d950319e9c57700cad6c2dff9c7
James Bradbury <unkoun98@gmail.com>
2024-12-04 22:49:26 +0000

    day 4 done

--- CodeAdvent23/CodeAdvent23.cpp
+++ CodeAdvent23/CodeAdvent23.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
-#include <regex>
+#include <algorithm>
 
 
 using namespace std;
@@ -18,7 +18,6 @@ void bubbleSort(vector<int>& v)
 		}
 	}
 }
-
 
 bool NoProblemFinder(vector<int> numbers, int tolerance)
 { // Guess we doin' recursion now... (Is this neccesary? Probably not!)
@@ -64,6 +63,40 @@ vector<string> GetStringsFromFile(const string fileName)
 	return inputLines;
 }
 
+//void RunNextFunctionInString(string& line, const vector<string>& operations, bool& writing, int& runningTotal)
+//{ // OK we doing this - but next time we using regex's
+//	int start = -1;
+//	int best = INT_MAX;
+//	string nextOp;
+//	for (string op : operations)
+//	{
+//		int temp = line.find(op);
+//		if (temp < best && (temp != -1))
+//		{
+//			start = temp;
+//			best = temp;
+//			nextOp = op;
+//		}
+//	}
+//	if (start == -1)		return;
+//	line = line.substr(start + nextOp.size(), line.size());
+//	int end = line.find(")");
+//	if (end == -1) return;
+//	if (nextOp == "do(")		writing = true;
+//	if (nextOp == "don't(")		writing = false;
+//	if (nextOp == "mul(" && writing)
+//	{
+//		string content = line.substr(0, end);
+//		if (CheckContentAcceptable(content, ','))
+//		{
+//			vector<int> numbers;
+//			GetIntsFromString(numbers, content, ',');
+//			runningTotal += numbers[0] * numbers[1]; // what happens if mul(i,i,i)? Hope it doesn't happen...
+//		}
+//	}
+//	line = line.substr(1, line.size()); // this is an awful, awful hack... But I'm sleepy and it fixes the error where a mul can be hidden inside a corrupt mul
+//	return RunNextFunctionInString(line, operations, writing, runningTotal);
+//}
 
 string GetStringFromFile(const string fileName)
 { 	// Originally I did this in case there was any muls across multiple lines, no hate plz ;-;
@@ -74,59 +107,78 @@ string GetStringFromFile(const string fileName)
 	myFile.close();
 	return inputLines;
 }
+
 void GetIntsFromString(vector<int>& v, const string line, const char delimiter)
 { // aww ye I got to use a function from an earlier challenge
 	int length = line.find(delimiter);
 	v.push_back(stoi(line.substr(0, length)));
 	if (length != -1) { return GetIntsFromString(v, line.substr(length + 1, line.size()), delimiter); }
 }
+
 bool CheckContentAcceptable(const string input, const char delimiter)
 { // This seems fine... I mean, unless there's any like mul(,00)
 	for (int i = 0; i < input.size(); i++) { if (input[i] != delimiter && !isdigit(input[i])) return false; }
 	return true;
 }
-void RunNextFunctionInString(string& line, const vector<string>& operations, bool& writing, int& runningTotal)
-{ // OK we doing this - but next time we using regex's
-	int start = -1;
-	int best = INT_MAX;
-	string nextOp;
-	for (string op : operations)
-	{
-		int temp = line.find(op);
-		if (temp < best && (temp != -1))
-		{
-			start = temp;
-			best = temp;
-			nextOp = op;
-		}
-	}
-	if (start == -1)		return;
-	line = line.substr(start + nextOp.size(), line.size());
-	int end = line.find(")");
-	if (end == -1) return;
-	if (nextOp == "do(")		writing = true;
-	if (nextOp == "don't(")		writing = false;
-	if (nextOp == "mul(" && writing)
-	{
-		string content = line.substr(0, end);
-		if (CheckContentAcceptable(content, ','))
-		{
-			vector<int> numbers;
-			GetIntsFromString(numbers, content, ',');
-			runningTotal += numbers[0] * numbers[1]; // what happens if mul(i,i,i)? Hope it doesn't happen...
-		}
-	}
-	line = line.substr(1, line.size()); // this is an awful, awful hack... But I'm sleepy and it fixes the error where a mul can be hidden inside a corrupt mul
-	return RunNextFunctionInString(line, operations, writing, runningTotal);
+
+
+void ReplaceCharsInString(string& line, const vector<char> startChars, const vector<char> endChars)
+{ // You can probably do this with regex?
+	for (int i = 0; i < startChars.size(); i++) replace(line.begin(), line.end(), startChars[i], endChars[i]);
 }
+
+bool IsWithinGridBounds(int x, int y, int minX, int minY, int maxX, int maxY)
+{
+	if (x >= maxX || x < minX || y < minY || y >= maxY) return false;
+	return true;
+}
+
+bool IsPath(const vector<string> input, int y, int x, int deltaY, int deltaX)
+{ // screw it, hacky solutions all the way down
+	int startNumber = input[y][x] - '0'; // This is a little silly, but because 0 = 42, this converts the ascii back into ints!
+	if (!IsWithinGridBounds(x + deltaX, y + deltaY, 0, 0, input[input.size() - 1].size(), input.size())) 			return false;
+	if (!IsWithinGridBounds(x - deltaX, y - deltaY, 0, 0, input[input.size() - 1].size(), input.size())) 			return false;
+	int number = input[y + deltaY][x + deltaX] - '0';
+	if (number - startNumber != 1) return  false;
+	number = input[y - deltaY][x - deltaX] - '0';
+	if (number - startNumber != 2) return  false;
+	return true;
+}
+
+int CountPaths(const vector<string> input, int y, int x)
+{
+	int count = 0;
+	for (int i = -1; i <= 1; i++)
+	{
+		for (int j = -1; j <= 1; j++)
+		{
+			if (i == 0 || j == 0) continue;
+			if (IsPath(input, y, x, i, j)) count++;
+		}
+	}
+
+	if (count == 2) return 1;
+	else return 0;
+
+}
+
 int main()
 {
-	string inputLines = GetStringFromFile("input.txt");
-	vector<string> operations;
+	vector<string> inputLines = GetStringsFromFile("input.txt");
+	vector <char> charsToReplace = { 'A', 'M', 'S' };
+	vector <char> intReplacements = { '0' , '1' , '2' };
+
+	for (int i = 0; i < inputLines.size(); i++) ReplaceCharsInString(inputLines[i], charsToReplace, intReplacements);
 	int runningTotal = 0;
-	vector <string> functions = { "mul(", "do(", "don't(" };
-	bool writing = true;
-	RunNextFunctionInString(inputLines, functions, writing, runningTotal);
+
+	for (int i = 0; i < inputLines.size(); i++)
+	{
+		for (int j = 0; j < inputLines[i].size(); j++)
+		{
+			if (inputLines[i][j] != '0')	continue;
+			runningTotal += CountPaths(inputLines, i, j);;
+		}
+	}
 	cout << runningTotal << endl;
 }
 
